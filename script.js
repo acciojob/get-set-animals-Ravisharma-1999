f8de8ba95ba5dcd83cd545e32fcbe29c876c5e99
Ravisharma-1999 <ravi829144@gmail.com>
2024-11-25 08:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-//complete this code
-class Animal {}
+// Animal Class
+class Animal {
+    constructor(species) {
+        this._species = species; // Private property convention
+    }
 
-class Dog extends Animal {}
+    // Getter for species
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    // Method to log a generic sound
+    makeSound() {
+        console.log("Some generic animal sound!");
+    }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat Class (Subclass of Animal)
+class Cat extends Animal {
+    constructor() {
+        super("Cat"); // Pass "Cat" as the species to the Animal constructor
+    }
+
+    // Method specific to Cat
+    purr() {
+        console.log("purr");
+    }
+}
+
+// Dog Class (Subclass of Animal)
+class Dog extends Animal {
+    constructor() {
+        super("Dog"); // Pass "Dog" as the species to the Animal constructor
+    }
+
+    // Method specific to Dog
+    bark() {
+        console.log("woof");
+    }
+}
+
+// Example Usage
+const genericAnimal = new Animal("Generic Animal");
+console.log(genericAnimal.species); // "Generic Animal"
+genericAnimal.makeSound();          // "Some generic animal sound!"
+
+const cat = new Cat();
+console.log(cat.species);           // "Cat"
+cat.makeSound();                    // "Some generic animal sound!"
+cat.purr();                         // "purr"
+
+const dog = new Dog();
+console.log(dog.species);           // "Dog"
+dog.makeSound();                    // "Some generic animal sound!"
+dog.bark();                         // "woof"
