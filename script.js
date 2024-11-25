e888ece445aebc51f516eb0cb773f45131373c7c
Ravisharma-1999 <ravi829144@gmail.com>
2024-11-25 08:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Animal Class
 class Animal {
     constructor(species) {
-        this._species = species; // Private property convention
+        this._species = species;
     }
 
     // Getter for species
@@ -11,14 +11,14 @@ class Animal {
 
     // Method to log a generic sound
     makeSound() {
-        console.log("Some generic animal sound!");
+        console.log(`The ${this._species} makes a sound`);
     }
 }
 
-// Cat Class (Subclass of Animal)
+// Cat Class
 class Cat extends Animal {
-    constructor() {
-        super("Cat"); // Pass "Cat" as the species to the Animal constructor
+    constructor(species) {
+        super(species);
     }
 
     // Method specific to Cat
@@ -27,10 +27,10 @@ class Cat extends Animal {
     }
 }
 
-// Dog Class (Subclass of Animal)
+// Dog Class
 class Dog extends Animal {
-    constructor() {
-        super("Dog"); // Pass "Dog" as the species to the Animal constructor
+    constructor(species) {
+        super(species);
     }
 
     // Method specific to Dog
@@ -39,17 +39,7 @@ class Dog extends Animal {
     }
 }
 
-// Example Usage
-const genericAnimal = new Animal("Generic Animal");
-console.log(genericAnimal.species); // "Generic Animal"
-genericAnimal.makeSound();          // "Some generic animal sound!"
-
-const cat = new Cat();
-console.log(cat.species);           // "Cat"
-cat.makeSound();                    // "Some generic animal sound!"
-cat.purr();                         // "purr"
-
-const dog = new Dog();
-console.log(dog.species);           // "Dog"
-dog.makeSound();                    // "Some generic animal sound!"
-dog.bark();                         // "woof"
+// Attach classes to the global window object for Cypress testing
+window.Animal = Animal;
+window.Cat = Cat;
+window.Dog = Dog;
